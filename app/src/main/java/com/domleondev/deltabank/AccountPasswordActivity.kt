9125909c41ec16d3073edc78879ff9b5316f5376
domleondev <contato.leon@live.com>
2025-11-28 09:39:07 -0300

    Telas de registro, endereço, senha do app e senha da conta criadas(em xml)

---
+++ app/src/main/java/com/domleondev/deltabank/AccountPasswordActivity.kt
@@ -0,0 +1,28 @@
+package com.domleondev.deltabank
+
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class AccountPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_account_password)
+
+        val passwordAccountButtonNext = findViewById<ImageView>(R.id.password_Account_Button_Back)
+
+        passwordAccountButtonNext.setOnClickListener {
+            finish()
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/domleondev/deltabank/RegisterActivity.kt
@@ -0,0 +1,35 @@
+package com.domleondev.deltabank
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.domleondev.deltabank.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_register)
+
+       val registerButtonNext = findViewById<AppCompatButton>(R.id.register_Button_Next)
+        val registerButtonBack = findViewById<ImageView>(R.id.register_Button_Back)
+
+        registerButtonNext.setOnClickListener {
+            intent = Intent(this, AddressActivity::class.java)
+            startActivity(intent)
+        }
+        registerButtonBack.setOnClickListener {
+            finish()
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
