238631d98a3c5a0a9ca9ec4aa0ce76d0f352e6e9
Lidia Yamamura <rinoayk@gmail.com>
2025-12-06 13:43:01 -0300

    Chat Bot Kontein implementado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,13 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+val properties = Properties()
+val localPropertiesFile = rootProject.file("local.properties")
+
+if (localPropertiesFile.exists()) {
+    properties.load(FileInputStream(localPropertiesFile))
+}
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -16,6 +26,12 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        buildConfigField(
+            "String",
+            "GEMINI_API_KEY",
+            "\"${properties.getProperty("GEMINI_API_KEY")}\""
+        )
     }
 
     buildTypes {
@@ -30,6 +46,7 @@ android {
 
     buildFeatures {
         viewBinding = true
+        buildConfig = true
     }
 
     compileOptions {
@@ -48,17 +65,12 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation("androidx.fragment:fragment-ktx:1.8.9")
     implementation(libs.androidx.fragment)
     implementation(libs.androidx.tools.core)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation(libs.androidx.lifecycle.viewmodel)
-    implementation(libs.androidx.lifecycle.livedata)
-    implementation(libs.retrofit)
-    implementation(libs.retrofit.gson)
-    implementation(libs.androidx.recyclerview)
 
     // Firebase (controlado pelo BOM)
     implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
@@ -66,8 +78,6 @@ dependencies {
     implementation("com.google.firebase:firebase-auth-ktx")
     implementation("com.google.firebase:firebase-firestore-ktx")
     implementation("com.google.firebase:firebase-analytics-ktx")
-
-
 
     // Coroutines (necessário para suspendCancellableCoroutine)
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
@@ -78,9 +88,17 @@ dependencies {
     implementation(libs.retrofit.gson)
     implementation(libs.androidx.recyclerview)
 
+    // CameraX
     implementation("androidx.camera:camera-camera2:1.4.0")
     implementation("androidx.camera:camera-lifecycle:1.4.0")
     implementation("androidx.camera:camera-view:1.4.0")
     implementation("androidx.camera:camera-extensions:1.4.0")
+
+    // ML Kit
     implementation("com.google.mlkit:barcode-scanning:17.3.0")
+
+    // ChatBot dependencies
+    implementation("com.google.code.gson:gson:2.11.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 }
