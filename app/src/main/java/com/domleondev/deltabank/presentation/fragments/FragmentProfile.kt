a5b6c33d120495931991b23deeacda78fd526938
Leonardo dos Santos <143965636+domleondev@users.noreply.github.com>
2025-12-08 15:01:20 -0300

    Revert "Fluxo de transferência finalizado ."

--- app/src/main/java/com/domleondev/deltabank/presentation/fragments/FragmentProfile.kt
+++ app/src/main/java/com/domleondev/deltabank/presentation/fragments/FragmentProfile.kt
@@ -1,14 +1,11 @@
 package com.domleondev.deltabank.presentation.fragments
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import com.domleondev.deltabank.R
-import com.domleondev.deltabank.presentation.activities.HomeActivity
 
 class FragmentProfile: Fragment() {
     override fun onCreateView(
@@ -17,16 +14,5 @@ class FragmentProfile: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_profile, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val fragmentProfileButtonBack =
-            view.findViewById<ImageView>(R.id.fragment_Profile_Button_Back)
-
-        fragmentProfileButtonBack.setOnClickListener {
-            val intent = Intent(requireContext(), HomeActivity::class.java)
-            startActivity(intent)
-        }
     }
 }
