c5d469dffbdf79d2163712f9ef492a773de8f295
Lidia Yamamura <rinoayk@gmail.com>
2025-12-14 17:31:40 -0300

    Repondo branchs perdidas

--- app/src/main/java/com/domleondev/deltabank/presentation/activities/MainActivity.kt
+++ app/src/main/java/com/domleondev/deltabank/presentation/activities/MainActivity.kt
@@ -5,14 +5,8 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.domleondev.deltabank.R
-import android.graphics.Color
-import android.os.Build
-import android.view.View
-import androidx.core.view.WindowCompat
-import androidx.core.view.WindowInsetsControllerCompat
+import com.domleondev.deltabank.presentation.util.setupTransparentStatusBarNoPadding
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,6 +17,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        setupTransparentStatusBarNoPadding(
+            rootViewId = R.id.main,
+            darkIcons = true
+        )
 
         buttonRegister = findViewById(R.id.btn_Register)
         buttonLogin = findViewById(R.id.btn_login)
@@ -37,48 +36,6 @@ class MainActivity : AppCompatActivity() {
         buttonLogin.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
-        }
-
-        //  Configuração universal de status bar transparente
-        val window = window
-
-// LÓGICA DE VERSÕES CORRIGIDA
-        when {
-            // Android 10 e anteriores (API < 30)
-            Build.VERSION.SDK_INT < Build.VERSION_CODES.R -> {
-                @Suppress("DEPRECATION")
-                window.decorView.systemUiVisibility =
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION // <--- ESSA É A CHAVE!
-
-                @Suppress("DEPRECATION")
-                window.statusBarColor = Color.TRANSPARENT
-                @Suppress("DEPRECATION")
-                window.navigationBarColor = Color.TRANSPARENT // <--- Força a cor aqui
-            }
-
-            // Android 11+ (API >= 30)
-            else -> {
-                // Este comando diz: "Não ajuste o layout pelas barras, deixe passar por trás"
-                WindowCompat.setDecorFitsSystemWindows(window, false)
-
-                val controller = WindowInsetsControllerCompat(window, window.decorView)
-                controller.isAppearanceLightStatusBars = true
-                // controller.isAppearanceLightNavigationBars = true // Descomente se os ícones da navbar sumirem
-
-                @Suppress("DEPRECATION")
-                window.statusBarColor = Color.TRANSPARENT
-                @Suppress("DEPRECATION")
-                window.navigationBarColor = Color.TRANSPARENT // <--- Garante a transparência
-            }
-        }
-
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val bars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(bars.left, 0, bars.right, 0)
-            insets
         }
     }
 }
